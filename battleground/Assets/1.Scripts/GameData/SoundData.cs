800732011e1ae1297527f5d4c317320e5b04010b
jungheol <jungheol@naver.com>
2023-12-03 22:46:59 +0900

    SoundData 스크립트 생성 및 SaveData 함수 생성

---
+++ battleground/Assets/1.Scripts/GameData/SoundData.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Xml;
+using System.IO;
+using UnityEditor.Timeline;
+
+/// <summary>
+/// 사운드 클립을 배열로 소지, 사운드 데이터를 저장하고 로드하고,
+/// 프리로딩을 갖고 있다.
+/// </summary>
+public class SoundData : BaseData {
+    public SoundClip[] soundClips = new SoundClip[0];
+    
+    private string clipPath = "Sound/";
+    private string xmlFilePath = "";
+    private string xmlFileName = "soundData.xml";
+    private string dataPath = "Data/soundData";
+    private static string SOUND = "sound";
+    private static string CLIP = "clip";
+
+    public SoundData() {}
+
+    public void SaveData() {
+        using (XmlTextWriter xml = new XmlTextWriter(xmlFilePath + xmlFileName, System.Text.Encoding.Unicode)) {
+            xml.WriteStartDocument();
+            xml.WriteStartElement(SOUND);
+            xml.WriteElementString("length", GetDataCount().ToString());
+            xml.WriteWhitespace("\n");
+
+            for (int i = 0; i < this.names.Length; i++) {
+                SoundClip clip = this.soundClips[i];
+                xml.WriteStartElement(CLIP);
+                xml.WriteElementString("id", i.ToString());
+                xml.WriteElementString("name", this.names[i]);
+                xml.WriteElementString("loops", clip.checkTime.Length.ToString());
+                xml.WriteElementString("maxvol", clip.maxVolume.ToString());
+                xml.WriteElementString("pitch", clip.pitch.ToString());
+                xml.WriteElementString("dopplerlevel", clip.dopplerLevel.ToString());
+                xml.WriteElementString("rollofmode", clip.rolloffMode.ToString());
+                xml.WriteElementString("mindistance", clip.minDistance.ToString());
+                xml.WriteElementString("maxdistance", clip.maxDistance.ToString());
+                xml.WriteElementString("sparialblend", clip.sparialBlend.ToString());
+                if (clip.isLoop == true) {
+                    xml.WriteElementString("loop", "true");
+                }
+                xml.WriteElementString("clippath", clip.clipPath);
+                xml.WriteElementString("clipname", clip.clipName);
+                xml.WriteElementString("checktimecount", clip.checkTime.Length.ToString());
+
+                string str = "";
+                foreach (var t in clip.checkTime) {
+                    str += t.ToString() + "/";
+                }
+                xml.WriteElementString("checktime", str);
+                str = "";
+                xml.WriteElementString("settimecount", clip.setTime.Length.ToString());
+                foreach (var t in clip.setTime) {
+                    str += t.ToString() + "/";
+                }
+                xml.WriteElementString("settime", str);
+                xml.WriteElementString("type", clip.playType.ToString());
+                
+                xml.WriteEndElement();
+            }
+            xml.WriteEndElement();
+            xml.WriteEndDocument();
+        }
+    }
+}
