7cdb74276e9097329727b542bc515892eff4fa5f
jungheol <jungheol@naver.com>
2023-12-07 23:58:24 +0900

    DataManager에 SoundData 추가

--- battleground/Assets/1.Scripts/Manager/DataManager.cs
+++ battleground/Assets/1.Scripts/Manager/DataManager.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour {
+    private static SoundData soundData = null;
     private static EffectData effectData = null;
     // Start is called before the first frame update
     void Start() {
         if(effectData == null) {
             effectData = ScriptableObject.CreateInstance<EffectData>();
             effectData.LoadData();
+        }
+
+        if (soundData == null) {
+            soundData = ScriptableObject.CreateInstance<SoundData>();
+            soundData.LoadData();
         }
     }
 
@@ -17,7 +23,14 @@ public class DataManager : MonoBehaviour {
             effectData = ScriptableObject.CreateInstance<EffectData>();
             effectData.LoadData();
         }
-
         return effectData;
+    }
+
+    public static SoundData SoundData() {
+        if (soundData == null) {
+            soundData = ScriptableObject.CreateInstance<SoundData>();
+            soundData.LoadData();
+        }
+        return soundData;
     }
 }
